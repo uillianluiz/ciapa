f6c2e5af0a7c12d3678ce81615b7ee92087f96f0
Uillian <uillianludwig@gmail.com>
2017-10-05 11:24:30 -0300

    update degradation file

--- src/app/ciapa/datatype/Degradation.ts
+++ src/app/ciapa/datatype/Degradation.ts
@@ -3,35 +3,35 @@
  */
 
 enum DegradationCPU {
-    Absent = 1,
-    Low = 1.01,
-    Moderate = 1.25,
-    High = 1.5
+    Absent = 1.00,
+    Low = 1.03,
+    Moderate = 1.15,
+    High = 1.33
 }
 
 enum DegradationMemory {
-    Absent = 1,
+    Absent = 1.00,
     Low = 1.07,
     Moderate = 1.64,
     High = 1.74
 }
 
 enum DegradationDisk {
-    Absent = 1,
-    Low = 1.3,
-    Moderate = 1.8,
-    High = 2.4
+    Absent = 1.00,
+    Low = 1.12,
+    Moderate = 1.82,
+    High = 2.25
 }
 
 enum DegradationCache {
-    Absent = 1,
+    Absent = 1.00,
     Low = 1.07,
     Moderate = 1.18,
     High = 1.26
 }
 
 enum DegradationAffinity {
-    Absent = 1,
+    Absent = 1.00,
     Low = 1.05,
     Moderate = 1.32,
     High = 1.57
