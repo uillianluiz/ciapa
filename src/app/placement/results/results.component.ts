c1e867600ae7b60842c9a9dafaba914e0807c004
Uillian Ludwig <uillianluiz@yahoo.com.br>
2017-11-17 23:56:25 -0200

    add export placement to csv

--- src/app/placement/results/results.component.ts
+++ src/app/placement/results/results.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { PlacementService } from '../../services/placement.service';
+import { Solution } from '../../ciapa/datatype/Solution';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-results',
@@ -7,15 +9,14 @@ import { PlacementService } from '../../services/placement.service';
   styleUrls: ['./results.component.scss']
 })
 export class ResultsComponent implements OnInit {
-
   public limitShort = 1075;
   public limitOverflow = 851;
 
   public isOverflow = window.innerWidth < this.limitOverflow;
   public showShort = window.innerWidth < this.limitShort;
-  constructor(public _placementService: PlacementService) { }
+  constructor(public _placementService: PlacementService) {}
 
-  ngOnInit() { }
+  ngOnInit() {}
 
   @HostListener('window:resize', ['$event'])
   onResize(event) {
@@ -29,5 +30,23 @@ export class ResultsComponent implements OnInit {
       this.isOverflow = false;
       this.showShort = false;
     }
+  }
+
+  export(solution) {
+    const filename = `${solution.algorithm}.csv`;
+    solution = solution.solution as Solution;
+    const data = [];
+    for (const pm of solution.PMs) {
+      const row = [pm.name];
+      for (const tier of pm.tiers) {
+        row.push(tier.name);
+      }
+      data.push(row.join(','));
+    }
+    const blob = new Blob([data.join('\n')], {
+      type: 'plain/text',
+      endings: 'native'
+    });
+    FileSaver.saveAs(blob, filename);
   }
 }
